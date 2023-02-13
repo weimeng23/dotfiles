48826c45ac4b2e472d33d5238b5509b2d0632cfc
Meng Wei <wmeng94@gmail.com>
2023-02-13 06:55:12 +0000

    vim/neovim config

--- install.sh
+++ install.sh
@@ -5,18 +5,37 @@
 # Last Modified Date: 06.02.2023
 # Last Modified By  : Meng Wei <wmeng94@gmail.com>
 
-# vim
-if [[ -f ~/.config/nvim/init.vim ]]; then
-    if [[ ! -d ~/.config/nvim ]];then
-        mkdir -p ~/.config/nvim
-    fi
-    mv ~/.config/nvim/init.vim ~/.config/nvim/init.vim.bak
-    cp init.vim ~/.config/nvim/init.vim
+# neo/vim
+# for vim, set neovim=0
+neovim=1
+if [[ $neovim == 1 ]];then
+    echo 'set neovim plug'
+    curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 else
-    if [[ ! -d ~/.config/nvim ]];then
-        mkdir -p ~/.config/nvim
+    echo 'set neovim plug'
+    sed -i "s/\~\/\.config\/nvim/\~\/\.vim/" init.vim
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+if [[ $neovim == 1 ]];then
+    echo 'set neovim config'
+    if [[ -f ~/.config/nvim/init.vim ]]; then
+        mv ~/.config/nvim/init.vim ~/.config/nvim/init.vim.bak
+        cp init.vim ~/.config/nvim/init.vim
+    else
+        if [[ ! -d ~/.config/nvim ]];then
+            mkdir -p ~/.config/nvim
+        fi
+        cp init.vim ~/.config/nvim/init.vim
     fi
-    cp init.vim ~/.config/nvim/init.vim
+else
+    echo 'set vim config'
+    if [[ -f ~/.vimrc ]]; then
+        mv ~/.vimrc ~/.vimrc.bak
+        cp init.vim ~/.vimrc
+    else
+        cp init.vim ~/.vimrc
+    fi
 fi
 
 # tmux
